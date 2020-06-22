6692f4d3265a9a4e5a64b0337934a56918d838a2
ronlalum <ronlalum@gmail.com>
2020-06-22 15:14:45 +0300

    upload

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Search() {
+    return (
+        <section className="searchbox-wrap">
+            <input type="text" className="searchbox" placeholder="Search Movie"/>
+        </section>
+    )
+}
+
+export default Search;
